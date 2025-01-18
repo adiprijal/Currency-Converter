d1927f542d39589bcfa9baf8656f86ec49c2da59
adiprijal <rizalaryan2@gmail.com>
2025-01-18 20:08:33 +0545

    refactor Navbar styles; update layout, borders, and image dimensions for improved aesthetics

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -5,9 +5,12 @@ import './Navbar.css';
 const Nav = () => {
   return (
     <nav>
-      <header className="App-header">
+      <header className="App-header" >
         <img src={logo} className="App-logo" alt="logo" style={ { width: '90px' , height : 'auto'}} />
       <h1>HeraPheri</h1>
+
+
+
       </header>
     </nav>
   );
