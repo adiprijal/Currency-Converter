ecddb877295cf2d75a5205546150149e46df6115
adiprijal <rizalaryan2@gmail.com>
2025-01-18 15:21:10 +0545

    refactor Navbar and Converter styles; adjust logo size, padding, and layout for responsiveness

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
   return (
     <nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" style={ { width: '90px' , height : 'auto'}} />
       <h1>HeraPheri</h1>
       </header>
     </nav>
